acb1d65ab1d28ee7e34d3294df80c35c71603b1b
Margo <margo@meowmeow>
2021-07-05 16:28:06 +0700

    button component completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from '../components';
 import { Htag } from '../components/Htags/Htag';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Meow</Htag>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='right'>Button</Button>
     </>
   );
 }
