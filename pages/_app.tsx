9114b04f04706d903c2553ad607307c31750cc4c
Margo <margo@meowmeow>
2021-07-05 13:40:19 +0700

    headings component completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,9 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <Head>
       <title>Top App</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
     </Head>
     <Component {...pageProps} />;
   </>;
